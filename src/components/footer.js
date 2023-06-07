84786aafeecf007fb4d76537febfaa44cafa879b
Pheello <molokolip@gmail.com>
2023-06-07 16:45:47 +0200

    'alxmkp'

---
+++ src/components/footer.js
@@ -0,0 +1,5 @@
+// src/components/footer.js
+import React from "react"
+export default function Footer(props) {
+ return <p>&copy; {props.copyrightYear} My Gatsby Site. All rights reserved.</p>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
-import * as React from "react"
+// src/pages/index.js
+import React from "react"
+import Footer from "../components/footer"
+import Appbar from "../components/appbar"
+import Card from "../components/card"
+//import Container from "../components/container"
+import { AppBar } from "@mui/material"
 
 export default function Home() {
-  return <div>Hello world!</div>
+ return (
+<div>
+ <Appbar />
+
+<Card />
+
+ <Footer />
+ </div>
+ )
 }
+//51653560
